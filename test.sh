df38f370fb5116c27640dd9765589fb1dfb6aff0
hyder28 <hyderalis.2017@mitb.smu.edu.sg>
2022-08-15 23:02:35 +0800

    update bash script

--- test.sh
+++ test.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-echo "verify libraries"
+echo "verify libraries..."
 python -c '
-import transformers;
 import librosa;
 import torch;
-import spacy;
 import pydub;
+'
+echo "verify models..."
+python -c '
+import spacy;
+spacy.load("./models/en_core_web_sm-3.4.0");
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer;
+model = Wav2Vec2ForCTC.from_pretrained("models/wav2vec2-large-960h",local_files_only=True)
 '
