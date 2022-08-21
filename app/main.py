ec59b2a86e910a36ab9ab6034842ee865deb481c
hyder28 <hyderalis.2017@mitb.smu.edu.sg>
2022-08-21 23:11:17 +0800

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,22 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 import uvicorn
 
-from app.utils import get_torch_device
+from app.utils import get_torch_device, SpacySM
 
 app = FastAPI()
+
+
+class TextRequest(BaseModel):
+    text: str
+
 
 @app.get("/")
 def root() -> dict:
     return {"message": "Lets process files!"}
+
 
 @app.get("/health")
 def check_health() -> dict:
     """
     Checks health
     """
-    torch_device = get_torch_device()
+    return {"status": "OK", "device": get_torch_device()}
 
-    return {"status": "OK", "Device": torch_device}
+
+@app.post("/text_topic")
+def get_topics(user_request_in: TextRequest):
+    spacy_model = SpacySM()
+    result = spacy_model.get_topic(user_request_in.text)
+
+    return result
 
 
 if __name__ == "__main__":
