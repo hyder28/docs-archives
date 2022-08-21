cf6d1897398caae4be39487b0d79008582413e9d
hyder28 <hyderalis.2017@mitb.smu.edu.sg>
2022-08-21 22:15:48 +0800

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# todo: create app
+from fastapi import FastAPI
+import uvicorn
+
+from app.utils import get_torch_device
+
+app = FastAPI()
+
+@app.get("/")
+def root() -> dict:
+    return {"message": "Lets process files!"}
+
+@app.get("/health")
+def check_health() -> dict:
+    """
+    Checks health
+    """
+    torch_device = get_torch_device()
+
+    return {"status": "OK", "Device": torch_device}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=5000)
--- app/utils/verify_torch.py
+++ app/utils/verify_torch.py
@@ -1,11 +1,7 @@
 import torch
-# setting device on GPU if available, else CPU
 
-def check_gpu():
-    torch_device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print('Using device:', torch_device)
-    print()
 
-    #Additional Info when using cuda
-    if torch_device.type == 'cuda':
-        print(torch.cuda.get_device_name(0))
+def get_torch_device():
+    torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    return torch_device
