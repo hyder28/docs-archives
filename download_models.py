6343044d0df923b2719bb663ce70e808ecef476d
hyder28 <hyderalis.2017@mitb.smu.edu.sg>
2022-08-22 10:59:03 +0800

    update main.py

--- download_models.py
+++ download_models.py
@@ -4,7 +4,8 @@ from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
 tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
 model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
 
-model.save_pretrained('./models/wav2vec2-base-960h-v2')
-tokenizer.save_pretrained('./models/wav2vec2-base-960h-v2')
+model.save_pretrained('./app/models/wav2vec2-base-960h')
+tokenizer.save_pretrained('./app/models/wav2vec2-base-960h')
 
 # spacy models
+#todo - add spacy model download here
